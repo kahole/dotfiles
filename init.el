0ce1fde95a8ffc3a8862ce2ddb6e39704990c998
Kristian Andersen Hole <kristian@hole.priv.no>
2018-12-01 02:18:16 +0100

    switches to use-package for all the packages, cleans up

--- init.el
+++ init.el
@@ -1,19 +1,52 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;(require 'package)
+;(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")
+        ("melpa" . "http://melpa.milkbox.net/packages/"))
+      package-archive-priorities
+      '(("melpa-stable" . 15)
+        ("gnu" . 10)
+        ("org" . 5)
+        ("melpa" . 0)))
 
+; For portability to emacs versions without use-package already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-; ---------------------------
-; [ Auto install packages ]
-; ---------------------------
-;; (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-(defvar my-packages '(haskell-mode go-mode spacemacs-theme moe-theme solarized-theme evil-commentary magit ace-jump-mode org-bullets org-present exec-path-from-shell company all-the-icons-ivy neotree doom-themes evil markdown-mode))
-(dolist (p my-packages)
-  (when (not(package-installed-p p))
-    (package-install p)))
+(require 'use-package)
+
+; Auto install packages
+(setq use-package-always-ensure t)
+
+; Auto update
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+; Packages
+
+(use-package magit)
+(use-package ace-jump-mode)
+
+(use-package markdown-mode)
+(use-package haskell-mode)
+(use-package go-mode)
+
+(use-package doom-themes)
+(use-package spacemacs-theme :defer t :init (load-theme 'spacemacs-light t))
+(use-package moe-theme)
+(use-package solarized-theme)
+;(use-package all-the-icons)
+(use-package all-the-icons-ivy)
+
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
 
 ; ---------------------------
 ; [ General settings ]
@@ -21,10 +54,9 @@
 
 (add-to-list 'default-frame-alist '(height . 66))
 (add-to-list 'default-frame-alist '(width . 160))
-
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-; When using a dark theme, turn on:
-;(add-to-list 'default-frame-alist '(ns-appearance . dark))
+; Options (light|dark) dark theme use light, vice versa
+(add-to-list 'default-frame-alist '(ns-appearance . light))
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -46,9 +78,6 @@
 
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
-;(require 'exec-path-from-shell)
-(exec-path-from-shell-initialize)
-
 (setq mac-option-key-is-meta t)
 (setq mac-right-option-modifier nil)
 
@@ -62,28 +91,30 @@
 ; [ Doc-View ]
 ; ---------------------------
 
+(setq doc-view-resolution 800)
+; Auto reload on file changed
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
 
 ; ---------------------------
 ; [ Dashboard ]
 ; ---------------------------
 
-(use-package dashboard)
-(dashboard-setup-startup-hook)
+(use-package dashboard
+  :config
+  (dashboard-setup-startup-hook)
 
-(setq dashboard-startup-banner 'logo)
+  (setq dashboard-startup-banner 'logo)
 
-(setq dashboard-banner-logo-title "Welcome to Emacs yo!")
+  (setq dashboard-banner-logo-title "Welcome to Emacs yo!")
 
-(setq dashboard-items '((recents  . 6)
-                        (bookmarks . 6)
-                        ;(projects . 5)
-                        (agenda . t)))
+  (setq dashboard-items '((recents  . 6)
+                          (bookmarks . 6)
+                          ;(projects . 5)
+                          (agenda . t)))
 
-(defun dashboard-insert-custom (x) (insert "Custom text"))
-(add-to-list 'dashboard-item-generators  '(custom . dashboard-insert-custom))
-(add-to-list 'dashboard-items '(custom) t)
-
+  (defun dashboard-insert-custom (x) (insert "Custom text"))
+  (add-to-list 'dashboard-item-generators  '(custom . dashboard-insert-custom))
+  (add-to-list 'dashboard-items '(custom) t))
 
 ; ---------------------------
 ; [ Org-Mode ]
@@ -92,12 +123,9 @@
 (setq org-latex-to-pdf-process '("texi2dvi --pdf --clean --verbose --batch %f"))
 
 (setq org-hierarchical-todo-statistics nil)
-
 (setq org-image-actual-width nil)
 (setq org-startup-with-inline-images t)
-
 (setq org-cycle-separator-lines -2)
-
 
 ; org babel
 (setq org-babel-python-command "python3")
@@ -107,21 +135,29 @@
  '((python . t) (js . t) (dot . t))
  )
 
+; truncate lines
+(add-hook 'org-mode-hook (lambda () (toggle-truncate-lines)))
 
 ; org pretty bullets
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(use-package org-bullets
+  :init
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 ; org-present
+(use-package org-present)
+
 (eval-after-load "org-present"
   '(progn
      (add-hook 'org-present-mode-hook
                (lambda ()
+                 (turn-off-evil-mode)
                  (org-present-big)
                  (org-display-inline-images)
                  (org-present-hide-cursor)
                  (org-present-read-only)))
      (add-hook 'org-present-mode-quit-hook
                (lambda ()
+                 (turn-on-evil-mode)
                  (org-present-small)
                  (org-present-show-cursor)
                  (org-present-read-write)))))
@@ -131,15 +167,17 @@
 ; [ Evil ]
 ; ---------------------------
 
-(use-package evil)
-(evil-mode 1)
+(use-package evil
+  :config
+  (evil-mode 1)
+  (setq evil-scroll-count 3))
 
-(setq evil-scroll-count 3)
+(use-package evil-commentary
+  :config
+  (evil-commentary-mode))
 
-; Evil commentary
-(evil-commentary-mode)
 ; ---------------------------
-; [ Key-Bindings ]
+; [ Evil Key-Bindings ]
 ; ---------------------------
 
 (global-set-key (kbd "M-j") (lambda() (interactive) (evil-scroll-line-down 3)))
@@ -166,109 +204,20 @@
 ; ---------------------------
 ; [ NeoTree ]
 ; ---------------------------
-(setq neo-smart-open t)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(use-package neotree
+  :config
+  (setq neo-smart-open t)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
 
 ; ---------------------------
 ; [ Company ]
 ; ---------------------------
 
-;(require 'company)
-(global-company-mode)
-(setq company-selection-wrap-around t)
-(define-key company-active-map [tab] 'company-complete)
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(compilation-message-face 'default)
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#657b83")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes '(spacemacs-light))
- '(custom-safe-themes
-   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "b9cbfb43711effa2e0a7fbc99d5e7522d8d8c1c151a3194a4b176ec17c9a8215" "291588d57d863d0394a0d207647d9f24d1a8083bb0c9e8808280b46996f3eb83" "30f7c9e85d7fad93cf4b5a97c319f612754374f134f8202d1c74b0c58404b8df" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" "2c88b703cbe7ce802bf6f0bffe3edbb8d9ec68fc7557089d4eaa1e29f7529fe1" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "ecba61c2239fbef776a72b65295b88e5534e458dfe3e6d7d9f9cb353448a569e" "9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "a156fcac344bbfdc979a5adf9cecf1c2de56c4c937549ae0571b7f11ad4fe6a9" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "ce3e6c12b48979ce89754884d913c7ecc8a7956543d8b09ef13abfab6af9aa35" "8891c81848a6cf203c7ac816436ea1a859c34038c39e3cf9f48292d8b1c86528" "a566448baba25f48e1833d86807b77876a899fc0c3d33394094cf267c970749f" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "b01b91ba9276deb39aa892c105a8644ef281b4d1804ab7c48de96e9c5d2aaa48" "4e21fb654406f11ab2a628c47c1cbe53bab645d32f2c807ee2295436f09103c6" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "2a1b4531f353ec68f2afd51b396375ac2547c078d035f51242ba907ad8ca19da" "7666b079fc1493b74c1f0c5e6857f3cf0389696f2d9b8791c892c696ab4a9b64" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "77c3f5f5acaa5a276ca709ff82cce9b303f49d383415f740ba8bcc76570718b9" "0846e3b976425f142137352e87dd6ac1c0a1980bb70f81bfcf4a54177f1ab495" "b5ecb5523d1a1e119dfed036e7921b4ba00ef95ac408b51d0cd1ca74870aeb14" "2af26301bded15f5f9111d3a161b6bfb3f4b93ec34ffa95e42815396da9cb560" "8b30636c9a903a9fa38c7dcf779da0724a37959967b6e4c714fdc3b3fe0b8653" default))
- '(fci-rule-color "#383a42")
- '(highlight-changes-colors '("#d33682" "#6c71c4"))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#fdf6e3" 0.25)
-    '("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2")))
- '(highlight-symbol-foreground-color "#586e75")
- '(highlight-tail-colors
-   '(("#eee8d5" . 0)
-     ("#B4C342" . 20)
-     ("#69CABF" . 30)
-     ("#69B7F0" . 50)
-     ("#DEB542" . 60)
-     ("#F2804F" . 70)
-     ("#F771AC" . 85)
-     ("#eee8d5" . 100)))
- '(hl-bg-colors
-   '("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342"))
- '(hl-fg-colors
-   '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
- '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
- '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#9ca0a4"))
- '(magit-diff-use-overlays nil)
- '(markdown-header-scaling t)
- '(nrepl-message-colors
-   '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
- '(org-agenda-files '("~/ntnu/todo.org"))
- '(package-selected-packages
-   '(use-package haskell-mode go-mode spacemacs-theme moe-theme solarized-theme evil-commentary magit ace-jump-mode org-bullets org-present exec-path-from-shell company all-the-icons-ivy neotree doom-themes evil markdown-mode))
- '(pos-tip-background-color "#eee8d5")
- '(pos-tip-foreground-color "#586e75")
- '(show-paren-mode t)
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
- '(term-default-bg-color "#fdf6e3")
- '(term-default-fg-color "#657b83")
- '(tool-bar-mode nil)
- '(vc-annotate-background "#f0f0f0")
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#50a14f")
-    (cons 40 "#688e35")
-    (cons 60 "#807b1b")
-    (cons 80 "#986801")
-    (cons 100 "#ae7118")
-    (cons 120 "#c37b30")
-    (cons 140 "#da8548")
-    (cons 160 "#c86566")
-    (cons 180 "#b74585")
-    (cons 200 "#a626a4")
-    (cons 220 "#ba3685")
-    (cons 240 "#cf4667")
-    (cons 260 "#e45649")
-    (cons 280 "#d2685f")
-    (cons 300 "#c07b76")
-    (cons 320 "#ae8d8d")
-    (cons 340 "#383a42")
-    (cons 360 "#383a42")))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   '(unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))
- '(xterm-color-names
-   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
- '(xterm-color-names-bright
-   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package company
+  :config
+  (global-company-mode)
+  (setq company-selection-wrap-around t)
+  (define-key company-active-map [tab] 'company-complete)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
